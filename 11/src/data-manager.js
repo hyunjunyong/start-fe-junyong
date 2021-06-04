decb8828c4dde1736ebc2054386c2abebfbbb6b0
hyunjunyong <hjy4649@naver.com>
2021-06-04 13:13:51 +0900

    Add docs

--- 11/src/data-manager.js
+++ 11/src/data-manager.js
@@ -2,8 +2,16 @@ function saveData(data) {
   console.log('todos:', data);
   localStorage.setItem('data', JSON.stringify(data));
 }
-function loadData() {
+function loadData(callback) {
   const dataString = localStorage.getItem('data') || '[]';
   return JSON.parse(dataString);
+  // fetch('http://localhost:3000.todos').then((response) => {
+  //   response.json().then((data) => {
+  // return(data);
+  //     callback(data);
+  //
+  // console.log('json data:', data);
+  //   });
+  // });
 }
 export { saveData, loadData };
